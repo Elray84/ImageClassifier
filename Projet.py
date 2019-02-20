9c4996375b13de3def48768a25d9c220ac11264c
Elray84 <elianraynaud@gmail.com>
2019-02-20 16:42:43 +0100

    Question 1 DONE

--- Projet.py
+++ Projet.py
@@ -1,32 +1,41 @@
 import numpy as np
 
-imgs = np.load("data/trn_img.npy")
-labels = np.load("data/trn_lbl.npy")
+trnImgs = np.load("data/trn_img.npy")
+trnLbls = np.load("data/trn_lbl.npy")
+
+devImgs = np.load("data/dev_img.npy")
+devLbls = np.load("data/dev_lbl.npy")
 
 import matplotlib.pyplot as plt
 
-repr = []
-
-for i in np.unique(labels):
-    repr.append(imgs[labels == i].mean(axis=0))
-
-repr = np.array(repr)
-print(repr.shape)
+def calculRepr(imgs, labels):
+    repr = []
+    for i in np.unique(labels):
+        repr.append(imgs[labels == i].mean(axis=0))
+    return np.array(repr)
 
 
-for j in range(10):
-    plt.figure()
-    img = repr[j].reshape(28,28)
-    plt.imshow(img, plt.cm.gray)
+def show(imgs):
+    for img in imgs:
+        plt.figure()
+        plt.imshow(img.reshape(28,28), plt.cm.gray)
+
     
-def test(img):
-    dmin = -1
-    classe = 0
+def computeClassAll(imgs, repr):
+    dToRepr = []
     for i in range(10):
-        if np.linalg.norm(img-repr[i]) < dmin or dmin == -1:
-            dmin = np.linalg.norm(img-repr[i])
-            classe = i
-    return classe
-
-testas = test(repr[9])
-print(testas)
+        dToRepr.append(np.linalg.norm(imgs-repr[i], axis=1))
+    dToRepr = np.array(dToRepr)
+    dToRepr = np.transpose(dToRepr)
+    results = np.argmin(dToRepr, axis=1)
+    return results
+    
+def failureRate(results, labels):
+    return (results != labels).sum() / labels.shape[0]
+    
+    
+def Question1():
+    repr = calculRepr(trnImgs, trnLbls)
+    results = computeClassAll(devImgs, repr)
+    rate = failureRate(results, devLbls)
+    print("Le taux d'exemples mal classes est de : ", rate)
